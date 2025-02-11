b4e20375c77b383b79045ed96ca7ecf4425a7601
nyakonda anesu <nyakondaanesu@gmail.com>
2025-02-11 13:19:34 +0200

    adding joinvideochat listener

--- video.js
+++ video.js
@@ -8,6 +8,25 @@ export function videoSock(socket, usersVideo, privateRoomsVideo) {
     usersVideo[googleUserId] = socket.id;
   });
 
+  socket.on("joinVideoChatRoom", (googleUserId) => {
+    console.log(`User ${googleUserId} looking for video match`);
+    let availableRoom = privateRoomsVideo.find(
+      (room) => room.users.length === 1
+    );
+
+    if (availableRoom && availableRoom.addUser(socket.id)) {
+      socket.join(availableRoom.id);
+      console.log(`Match found! Room: ${availableRoom.id}`);
+      io.to(availableRoom.id).emit("vidoeMatchFound");
+    } else {
+      const newRoom = new Room(generateUniqueId());
+      newRoom.addUser(socket.id);
+      privateRoomsVideo.push(newRoom);
+      socket.join(newRoom.id);
+      console.log(`New video room created: ${newRoom.id}`);
+    }
+  });
+
   // Fix: Remove undefined 'data' reference in console logs
   socket.on("offer", (offer) => {
     const userRoom = privateRoomsVideo.find((room) =>
