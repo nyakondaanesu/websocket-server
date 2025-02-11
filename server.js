76483b1ac93b71830348148994e6c935015350a6
nyakonda anesu <nyakondaanesu@gmail.com>
2025-02-11 13:41:01 +0200

    passing in missing arguements generateuniqueID

--- server.js
+++ server.js
@@ -73,14 +73,7 @@ io.on("connection", (socket) => {
     console.log(`✅ User authenticated: ${googleUserId}`);
   });
 
-  videoSock(
-    socket,
-    VideoUsers,
-    privateRoomsVideo,
-    Room,
-    generateUniqueId(),
-    io
-  );
+  videoSock(socket, VideoUsers, privateRoomsVideo, Room, generateUniqueId, io);
 
   socket.on("joinPrivateChat", (googleUserId) => {
     let availableRoom = privateRooms.find((room) => room.users.length === 1);
